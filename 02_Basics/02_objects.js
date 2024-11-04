cf90f06a7508c182b21ff081002404faac09c19e
codeby-gaurav <gauravtiwri8178@gmail.com>
2024-11-04 23:39:51 +0530

    objects

---
+++ 02_Basics/02_objects.js
@@ -0,0 +1,28 @@
+const myobj = new Object() //SIngleton object
+
+const mysyb = Symbol("dingdong")
+
+const obj2 = {
+    name : "Aman",
+    age: 52,
+    title: "teacher",
+    [mysyb]: "dhinkichika",
+    skills: {
+        python:"basics",
+        score: 2,
+        java: "intermediate",
+        c:"masterd"
+
+    },
+    scorecard:[2,4,5,6,7,8]
+};
+
+console.log(obj2.age) //not a proper way 
+console.log(obj2["name"]) //proper way
+console.log(`my skills are ${obj2.skills}`)
+
+obj2.age = 42
+Object.freeze(obj2) //can not make changes in object
+obj2.age = 68
+
+console.log(obj2.age)
